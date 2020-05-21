e1b257997e9b69fc01eff97dd40e7783dbcc12b0
Beau Dobbin <beau@beaunus.com>
2020-05-21 16:53:12 +0900

    feature: add orders of magnitude to benchmark

--- src/benchmark.ts
+++ src/benchmark.ts
@@ -1,29 +1,31 @@
 import * as _ from "lodash";
 import Benchmark from "benchmark";
 
-const suite = new Benchmark.Suite();
+const MAX_INPUT_SIZE = 1000;
 
-const INPUT_SIZE = 1000;
+for (let inputSize = 1; inputSize <= MAX_INPUT_SIZE; inputSize *= 10) {
+  const suite = new Benchmark.Suite();
+  console.group(`inputSize: ${inputSize}`);
+  const array = Array.from({ length: inputSize }, () => Math.random());
 
-const array = Array.from({ length: INPUT_SIZE }, () => Math.random());
-
-// add tests
-suite
-  .add("built-in", function () {
-    const thing = array.map((x) => x * 2);
-  })
-  .add("Lodash", function () {
-    const thing = _.map(array, (x) => x * 2);
-  })
-  // add listeners
-  .on("cycle", function (event: Benchmark.Event) {
-    console.log(String(event.target));
-  })
-  .on("complete", function () {
-    console.log(
-      "Fastest is " +
-        suite.filter("fastest").map(({ name }: { name: string }) => name)
-    );
-  })
-  // run async
-  .run({ async: true });
+  // add tests
+  suite
+    .add("built-in", function () {
+      const thing = array.map((x) => x * 2);
+    })
+    .add("Lodash", function () {
+      const thing = _.map(array, (x) => x * 2);
+    })
+    // add listeners
+    .on("cycle", function (event: Benchmark.Event) {
+      console.log(String(event.target));
+    })
+    .on("complete", function () {
+      console.log(
+        "Fastest is " +
+          suite.filter("fastest").map(({ name }: { name: string }) => name)
+      );
+    })
+    .run({ async: false });
+  console.groupEnd();
+}
