98a85710960144e324fcaa82a1e512ed025fed51
Beau Dobbin <beau@beaunus.com>
2022-01-29 20:13:26 +0900

    wip

--- src/matthewEffect.ts
+++ src/matthewEffect.ts
@@ -1,23 +1,67 @@
 /* eslint-disable jest/require-hook */
+import { plot } from "asciichart";
 import _ from "lodash";
 
-const NUM_PLAYERS = 10;
+const NUM_PLAYERS = 20;
 const balances = Array.from({ length: NUM_PLAYERS }, () => 100);
 
 let numRounds = 0;
-while (balances.filter((balance) => balance > 0).length > 1) {
-  const indicesWithPositiveBalance = balances
-    .map((_value, index) => index)
-    .filter((index) => balances[index]);
-  const player1Index = _.sample(indicesWithPositiveBalance) ?? 0;
-  const player2Index =
-    _.sample(
-      indicesWithPositiveBalance.filter((index) => index !== player1Index)
-    ) ?? 0;
-  ++balances[player1Index];
-  --balances[player2Index];
-  if (numRounds++ % 10000 === 0)
-    console.log(JSON.stringify({ amounts: balances }));
+
+async function main() {
+  while (balances.filter((balance) => balance > 0).length > 1) {
+    if (numRounds++ % 100 === 0) {
+      await sleep(10);
+      console.clear();
+      console.log(
+        plot(
+          Array.from(
+            { length: 101 },
+            (_v, k) =>
+              balances.filter((x) => x >= k * 10 && x < (k + 1) * 10).length
+          ),
+          { height: _.clamp(NUM_PLAYERS, 0, 30), max: NUM_PLAYERS }
+        )
+      );
+    }
+    const indicesWithPositiveBalance = balances
+      .map((_value, index) => index)
+      .filter((index) => balances[index]);
+    const player1Index = _.sample(indicesWithPositiveBalance) ?? 0;
+    const player2Index =
+      _.sample(
+        indicesWithPositiveBalance.filter((index) => index !== player1Index)
+      ) ?? 0;
+    ++balances[player1Index];
+    --balances[player2Index];
+
+    if (balances[player2Index] === 0) {
+      const donorIndex =
+        _.sample(
+          balances
+            .map((_value, index) => index)
+            .filter((index) => balances[index] > 1)
+        ) ?? 0;
+      ++balances[player2Index];
+      --balances[donorIndex];
+    }
+  }
+
+  console.log(
+    plot(
+      Array.from(
+        { length: 101 },
+        (_v, k) =>
+          balances.filter((x) => x >= k * 10 && x < (k + 1) * 10).length
+      ),
+      { height: 10 }
+    )
+  );
 }
 
-console.log({ amounts: balances, numRounds });
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+main();
